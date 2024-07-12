5799949b639c02ceea71d3656fc8918f9599f6fd
Triana Devia <trianadevia@gmail.com>
2024-07-12 01:25:42 -0300

    Modifico Home

--- js/script.js
+++ js/script.js
@@ -1,91 +1,30 @@
-const containerCarrusel = document.getElementById('container__carrusel');
-const carrusel = document.getElementById('carrusel');
-const buttonLeft = document.getElementById('button__navigation--less');
-const buttonRight = document.getElementById('button__navigation--more');
+//CARRUSEL
+const sliders = document.querySelectorAll('.slider');
+let currentIndex = 0;
 
-const slider = document.querySelectorAll('.slider');
+function goToSlide(index) {
+  currentIndex = index;
+  updateSliderPosition();
+  const slides = document.querySelectorAll('.slider__text');
+    slides.forEach((slider__text, i) => {
+        slider__text.classList.toggle('active', i === currentIndex);
+      });
+}
 
-const rootStyles = document.documentElement.style;
+function updateSliderPosition() {
+  const translateX = -currentIndex * 100;
+  document.querySelector('.slider-container').style.transform = `translateX(${translateX}%)`;
+}
 
-let slideCounter = 0;
-let isInTransition = false;
+setInterval(moveToNextSlide, 5000);
 
-const DIRECTION = {
-  RIGHT: 'RIGHT',
-  LEFT: 'LEFT'
-};
+function moveToNextSlide() {
+  currentIndex = (currentIndex + 1) % sliders.length;
+  updateSliderPosition();
+}
 
-const getTransformValue = () =>
-  Number(rootStyles.getPropertyValue('--slide-transform').replace('px', ''));
+//ANIMACION DE TEXTO BANNER
 
-const reorderSlide = () => {
-  const transformValue = getTransformValue();
-  rootStyles.setProperty('--transition', 'none');
-  if (slideCounter === slider.length - 1) {
-    carrusel.appendChild(carrusel.firstElementChild);
-    rootStyles.setProperty(
-      '--slide-transform',
-      `${transformValue + slider[slideCounter].scrollWidth}px`
-    );
-    slideCounter--;
-  } else if (slideCounter === 0) {
-    carrusel.prepend(carrusel.lastElementChild);
-    rootStyles.setProperty(
-      '--slide-transform',
-      `${transformValue - slider[slideCounter].scrollWidth}px`
-    );
-    slideCounter++;
-  }
-  isInTransition = false;
-};
-
-const moveSlide = direction => {
-  if (isInTransition) return;
-  const transformValue = getTransformValue();
-  rootStyles.setProperty('--transition', 'transform 1s');
-  isInTransition = true;
-  if (direction === DIRECTION.LEFT) {
-    rootStyles.setProperty(
-      '--slide-transform',
-      `${transformValue + slider[slideCounter].scrollWidth}%`
-    );
-    slideCounter--;
-  } else if (direction === DIRECTION.RIGHT) {
-    rootStyles.setProperty(
-      '--slide-transform',
-      `${transformValue - slider[slideCounter].scrollWidth}px`
-    );
-    slideCounter++;
-  }
-};
-
-buttonRight.addEventListener('click', () => moveSlide(DIRECTION.RIGHT));
-buttonLeft.addEventListener('click', () => moveSlide(DIRECTION.LEFT));
-
-carrusel.addEventListener('transitionend', reorderSlide);
-
-reorderSlide();
-
-//autoplay
-
-const autoplayInterval = 5000; // 5 segundos en milisegundos
-let autoplayTimer;
-
-const startAutoplay = () => {
-  autoplayTimer = setInterval(() => {
-    moveSlide(DIRECTION.RIGHT); // Llama a tu función para avanzar a la siguiente diapositiva
-  }, autoplayInterval);
-};
-
-const stopAutoplay = () => {
-  clearInterval(autoplayTimer);
-};
-
-// Inicia el autoplay cuando cargue la página
-startAutoplay();
-
-// Detén el autoplay cuando el usuario interactúe con el carrusel (por ejemplo, al hacer clic en una diapositiva)
-carrusel.addEventListener('click', stopAutoplay);
 
 // MENU HAMBURGUESA
 

8a006317c5cd181245744cf37eccb6be89565fdd
Triana Devia <trianadevia@gmail.com>
2024-07-29 20:40:24 -0300

    Agrego footer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//CARRUSEL
 const btnLeft = document.querySelector(".btn-left"),
       btnRight = document.querySelector(".btn-right"),
       slider = document.querySelector("#sliders"),
@@ -68,3 +69,27 @@ window.addEventListener("scroll", function() {
     let header = document.querySelector("header");
     header.classList.toggle("abajo", window.scrollY>0)
 })
+
+//ENVIAR FORMULARIO
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_x4fhw5s';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Mensaje enviado!');
+      location.reload();
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
